8175969997d703485b1c24b2ba50e0630a2e3680
Unknown <s.gsaenger@gmx.de>
2018-04-28 15:48:50 +0200

    inital fail

---
+++ PirateArena/Assets/Scripts/StringCollection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringCollection {
+
+
+
+    //PLAYER 1
+    public const string VERTICAL = "Vertical";
+    public const string Horizontal = "Horizontal";
+    //PLAYER 2
+    public const string VERTICAL2 = "Vertical2";
+    public const string HORIZONTAL2 = "Horizontal2";
+    //TAGS
+    public const string Player1 = "Player1";
+    public const string Player2 = "Player2";
+    //
+
+
+
+
+
+
+}
