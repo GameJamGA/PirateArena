150eee4bd7967459343efc961f4d25dc9e28e539
Unknown <andreas.edmeier@web.de>
2018-04-29 12:04:37 +0200

    Fire input auslesen

--- PirateArena/Assets/Scripts/ShipManager.cs
+++ PirateArena/Assets/Scripts/ShipManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ShipManager : MonoBehaviour {
 
 	public int index = 0; //index of the ship. is used for input controle
@@ -9,9 +10,18 @@ public class ShipManager : MonoBehaviour {
 
     private GameManager gm;
 
-    [SerializeField] float Life;
+    [SerializeField] float life;
     [SerializeField] float movementSpeed;
     [SerializeField] float rotationSpeed;
+    [SerializeField] float MaxCD;
+    [SerializeField] float BulletSpeed;
+
+    [SerializeField] int MaxPlayer = 2;
+
+    int right = 0;
+    float rightTimer = 0;
+    int left = 0;
+    float leftTimer = 0;
 
     float horizontalAxis; //is used for rotation
 
@@ -25,6 +35,20 @@ public class ShipManager : MonoBehaviour {
 
 	void Update () {
 		Move(Time.deltaTime);
+
+        if (right == 2)
+            rightTimer += Time.deltaTime;
+        if (left == 2)
+            leftTimer += Time.deltaTime;
+
+        if (rightTimer >= MaxCD) {
+            right = 0;
+            rightTimer = 0;
+        }
+        if (leftTimer >= MaxCD) {
+            left = 0;
+            leftTimer = 0;
+        }
 	}
 
 	void FixedUpdate() {
@@ -35,9 +59,34 @@ public class ShipManager : MonoBehaviour {
         switch (index) { //prosesses input dependent of player index
             case 0:
                 horizontalAxis = -Input.GetAxis(StringCollection.Horizontal);
+
+                if (right == 0 && Input.GetAxis(StringCollection.VERTICAL) < 0) {
+                    right = 1; //TODO: Srage right side
+                }else if (left == 0 && Input.GetAxis(StringCollection.VERTICAL) > 0) {
+                    left = 1; //TODO: Stage left side
+                }else if (right == 1 && Input.GetAxis(StringCollection.VERTICAL) >= 0) {
+                    right = 2; //TODO: Fire right
+                }else if (left == 1 && Input.GetAxis(StringCollection.VERTICAL) <= 0) {
+                    left = 2; //TODO: Fire left
+                }
+
                 break;
             case 1:
                 horizontalAxis = -Input.GetAxis(StringCollection.HORIZONTAL2);
+
+                if (right == 0 && Input.GetAxis(StringCollection.VERTICAL2) < 0)  {
+                    right = 1; //TODO: Srage right side
+                }
+                else if (left == 0 && Input.GetAxis(StringCollection.VERTICAL2) > 0) {
+                    left = 1; //TODO: Stage left side
+                }
+                else if (right == 1 && Input.GetAxis(StringCollection.VERTICAL2) >= 0) {
+                    right = 2; //TODO: Fire right
+                }
+                else if (left == 1 && Input.GetAxis(StringCollection.VERTICAL2) <= 0) {
+                    left = 2; //TODO: Fire left
+                }
+
                 break;
             default:
                 horizontalAxis = 0; //if player index is not handled coretly
@@ -52,9 +101,18 @@ public class ShipManager : MonoBehaviour {
     }
 
     public void OnHit(float damage = 1) { //will decrese the live of the player. it will report its death if it reaches 0 to gamemanager
-        Life -= damage;
-        if (Life <= 0) {
+        life -= damage;
+        if (life <= 0) {
             gm.SetGameOver();
         } //TODO: else report to UI
+    }
+
+    bool SetIndex (int i) {
+        if (i < 0)
+            return false;
+        if (i >= MaxPlayer)
+            return false;
+        index = i;
+        return true;
     }
 }
