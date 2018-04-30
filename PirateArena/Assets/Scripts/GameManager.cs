8fc582b1b504aa7e7cf00976b20460927b6372ea
Unknown <s.gsaenger@gmx.de>
2018-04-30 10:29:52 +0200

    Explosion now visible

--- PirateArena/Assets/Scripts/GameManager.cs
+++ PirateArena/Assets/Scripts/GameManager.cs
@@ -68,7 +68,7 @@ public class GameManager : MonoBehaviour {
 
     private void AskForRestart()
     {
-
+        StartGame();
     }
 
     public void SetGameOver()
@@ -84,13 +84,13 @@ public class GameManager : MonoBehaviour {
         {
             players[i] = Instantiate(shipPrefab, spawnPosition[i].transform.position, spawnPosition[i].transform.rotation);
             players[i].GetComponent<ShipManager>().index = i;
-            
-
         }
 
        
     }
 
+
+    //öffentliche funktion, die den Windvektor an alle Objekte übergeben kann
     public Vector2 GetNormalizedWind()
     {
         normalizedWind = myWind.normalized;
