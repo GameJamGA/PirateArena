6efd29f9ae670810b28705b2226e89ee9b257aab
Unknown <s.gsaenger@gmx.de>
2018-04-30 13:08:11 +0200

    Clouds added

---
+++ PirateArena/Assets/Scripts/CloudManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudManager : MonoBehaviour {
+
+    [SerializeField] GameObject clouds;
+    GameManager myGM;
+
+    private float movespeed = 2;
+
+    private void Start()
+    {
+        myGM = GameObject.Find(StringCollection.GAMEMANAGER).GetComponent<GameManager>();
+    }
+
+    private void FixedUpdate()
+    {
+        clouds.transform.position += new Vector3(myGM.GetNormalizedWind().x, myGM.GetNormalizedWind().y, 0) * movespeed;
+    }
+
+    
+
+}
